340a8dbdd393b6fdf87eee8da916c627671a42fb
George Klees <gksharkboy@gmail.com>
2016-03-10 23:56:07 -0500

    Cleaned up stuff, finalized organization, started on content.
    Need registration still.

---
+++ core/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Database authentication
+define("HOSTNAME", "localhost");
+define("USERNAME", "www-data");
+define("PASSWORD", ";lkjfdsa");
+define("DATABASE", "scout");
+define("REGISTRATION", "yes");
+
+?>
