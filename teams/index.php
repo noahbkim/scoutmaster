3d4ce94b1931c949667de1decca7323b5a4a50be
Marionumber1 <gksharkboy@gmail.com>
2016-03-12 12:23:43 -0500

    Make teams page list teams

--- teams/index.php
+++ teams/index.php
@@ -4,6 +4,27 @@ include_once "../core/session.php";
 include_once "../core/team.php";
 
 start();
-overview();
+$teams = overview();
 
 ?>
+
+<html>
+    <body>
+        <table border="1">
+		
+		<tr>
+		<td>Team ID</td>
+		<td>Team name</td>
+		</tr>
+		
+		<?php
+		foreach ($teams as $team){
+			echo "<tr>";
+			echo "<td>" . $team["team_id"] . "</td>";
+			echo "<td>" . $row["team_name"] . "</td>";
+			echo "</tr>";
+		}
+		?>
+		</table>
+    </body>
+</html>
