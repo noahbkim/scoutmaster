867e73a37a9b2a0c11091079907fa8f712a37987
Marionumber1 <gksharkboy@gmail.com>
2016-03-11 23:40:21 -0500

    Don't case 500 error for DB fail

--- core/list.php
+++ core/list.php
@@ -27,7 +27,7 @@ if (!auth()) {
 		$password = "Teque449";
 		$dbname = "teamdb";
 		$conn = new mysqli($servername, $username, $password, $dbname);
-		if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+		if ($conn->connect_error) { echo "DB connection failed"; }
 		
 		// Query each team's name
 		$sql = "SELECT teamname, teamid FROM teams";
