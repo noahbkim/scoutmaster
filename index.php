fba8146bddd38fdce49527a35a239f86aabeace6
Noah Kim <noahbkim@gmail.com>
2016-03-11 21:50:35 -0500

    Moved the login to the index.

--- index.php
+++ index.php
@@ -3,10 +3,6 @@
 include_once "core/auth.php";
 session_start();
 
-if (!auth()) {
-    header("Location: login.php");
-}
-
 ?>
 
 <html>
@@ -15,9 +11,27 @@ if (!auth()) {
         <?php include "template/head.php" ?>
     </head>
     <body>
+        
+    <?php 
+    
+    if (auth()) { ?>
+    
         <h1>Scoutmaster</h1>
         <p>Scoutmaster is a comprehensive FRC scouting tool. It also happens to be the first iteration of the scoutmaster software family to ever be functionally completed and deployed! The current models are set up for the 2016 FRC game, Stronghold.</p>
         <a href="core/logout.php">Logout</a>
+        
+    <?php } else { ?>
+          
+        <form action="core/login.php" method="post" name="login">
+            <input type="password" name="secret"/>
+            <input type="submit" value="Submit">
+        </form>
+        
+    <?php } 
+        
+        
+    ?>
+    
     </body>
 </html>
 
