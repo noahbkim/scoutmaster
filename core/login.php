ed5ab3eed25006a1eb14d89461269f3a45bfc92d
Noah Kim <nokim@stonefly.mbhs.edu>
2016-03-11 21:42:30 -0500

    Fixed some small stuff.

--- core/login.php
+++ core/login.php
@@ -3,26 +3,27 @@
 // Include and start session
 include_once "config.php";
 include_once "legacy.php";
-start_session();
+session_start();
 
 // Check if the credentials are set
-if ($_POST["secret"])) {
+if ($_POST["secret"]) {
 
     // Grab the credentials
     $secret = $_POST["secret"];
     $secret_hash = hash("sha512", $secret);
-    
+
     // Try to log in
     if (hash_equals($secret_hash, SECRET_HASH)) {
-        
+
         // Set the session secret
         $_SESSION["secret"] = $secret_hash;
-    
+        header("Location: /scout/index.php");
+
     } else {
-    
+
         // Send back to the login page with error
         header("Location: /scout/login.php?error=" . urlencode("secret"));
-    
+
     }
 
 }
