fa5a9702fa7ab401b10877f0e786741c31e97a10
Noah Kim <noahbkim@gmail.com>
2016-04-07 10:28:05 -0500

    Clearing everything to start a Django project

--- template/head.php
+++
@@ -1,3 +0,0 @@
-<link href="/scout/styles.css" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
-
--- template/navigation.php
+++
@@ -1,4 +0,0 @@
-<ul id="navigation">
-    <li style="float: left;"><a href="/scout/">Scoutmaster</a></li>
-    <li><a href="/scout/teams/">Teams</a></li>
-</ul>
