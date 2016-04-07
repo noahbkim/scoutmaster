4d5ddddb059a4e4100b58883bed875c6f78dda95
Noah Kim <nokim@stonefly.mbhs.edu>
2016-04-07 16:01:06 -0400

    Everything is happy

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,7 @@
 from django import http
 from django.shortcuts import render
 from django.template import loader
-from . import model
+from . import models
 
 import scraper
 import requests
@@ -10,8 +10,10 @@ import requests
 def index(request):
 
     matches = []
-    for data in scraper.scrape("event/2016chcmp"):
-        matches.append(model.Match.load(data))
+    for data in scraper.scrape("event/2016chcmp/matches"):
+        matches.append(models.Match.load(data))
+
+    matches = list(sorted(matches, key=lambda m: m.number))
     
     context = {"matches": matches}
     return render(request, "dashboard/index.html", context)
