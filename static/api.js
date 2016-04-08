48a0fb7e7aaf64406a5ec398129ff18f6f905b2d
Noah Kim <nokim@stonefly.mbhs.edu>
2016-04-08 12:00:35 -0400

    Everything works

--- static/api.js
+++ static/api.js
@@ -1,11 +1,41 @@
-function scrape(path) {
+function scrape(path, callback) {
     var request = new XMLHttpRequest();
     var url = "/scout/scrape?request=" + path;
-    request.open("GET", url, false);
+    request.open("GET", url, true);
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            var data = JSON.parse(request.responseText);
+            callback(data);
+        }
+    }
     request.send();
-    if (request.readyState == 4 && request.status == 200) {
-        var data = JSON.parse(request.responseText);
-        return data;
+}
+
+function scrapeAll(paths, callback) {
+    var responses = {}
+    callback = callback;
+    for (i in Object.keys(paths)) {
+        var request = new XMLHttpRequest();
+        request.name = Object.keys(paths)[i];
+        request.path = paths[request.name];
+        var url = "/scout/scrape?request=" + request.path;
+        request.open("GET", url, true);
+        request.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    try {
+                        responses[this.name] = JSON.parse(this.responseText);
+                    } catch(error) {}
+                } else {
+                    responses[this.name] = null;
+                }
+                for (i in Object.keys(paths)) {
+                    if (!responses.hasOwnProperty(Object.keys(paths)[i])) return;
+                }
+                callback(responses);
+            }
+        }
+        request.send();
     }
 }
 
@@ -18,13 +48,12 @@ function sorted(list, key) {
     return out;
 }
 
-function getCurrentEvent(team) {
-    var now = new Date(Date.now());
-    data = scrape("team/" + team + "/" + now.getFullYear() + "/events");
+function getCurrentEvent(events) {
+    var now = new Date();
     var i = 0;
-    var next = data[0];
-    while (i < data.length) {
-        var event = data[i];
+    var next = events[0];
+    while (i < events.length) {
+        var event = events[i];
         var end = new Date(event.end_date.replace("-", "/").replace("-", "/"));
         if (now < end) next = event;
         i++;
@@ -34,6 +63,34 @@ function getCurrentEvent(team) {
     return next;
 }
 
+function getCurrentMatch(matches) {
+    var now = new Date();
+    var i = 0;
+    var next = matches[0];
+    while (i < matches.length) {
+        var match = matches[i];
+        var start = new Date(match.time*1000);
+        next = match;
+        if (now < start) break;
+        i++;
+    }
+    return next;
+}
+
+function getLastMatch(matches) {
+    var now = new Date();
+    var i = 0;
+    var last = matches[0];
+    while (i < matches.length) {
+        var match = matches[i];
+        var start = new Date(match.time*1000);
+        if (now < start) break;
+        last = match;
+        i++;
+    }
+    return last;
+}
+
 function getMatches(event, team) {
     var data = scrape("event/" + event + "/matches");
     if (team !== undefined) var matches = scrape("team/" + team + "/event/" + event + "/matches");
@@ -41,11 +98,11 @@ function getMatches(event, team) {
     return sorted(matches, "match_number");
 }
 
-function getRanking(event, team) {
-    var data = scrape("event/" + event + "/rankings");
+function getRanking(rankings, team) {
     var result = [];
-    for (var i in data) if (data[i][1] == team.substring(3)) result = data[i]; 
+    for (var i in rankings) if (rankings[i][1] == team.substring(3)) result = rankings[i]; 
     var map = {};
-    for (var i in data[0]) map[data[0][i]] = result[i];
+    for (var i in rankings[0]) map[rankings[0][i]] = result[i];
     return map;
 }
+
--- static/util.js
+++ static/util.js
@@ -2,6 +2,15 @@ function twoDigit(string) {
     return ("00" + string).substr(-2);
 }
 
-function formatDigitalTime(date) {
-    return twoDigit(date.getHours()) + ":" + twoDigit(date.getMinutes()) + ":" + twoDigit(date.getSeconds());
+function formatDigitalTime(date, seconds) {
+    var out = twoDigit(date.getHours()) + ":" + twoDigit(date.getMinutes());
+    if (seconds === true) out += ":" + twoDigit(date.getSeconds());
+    return out;
+}
+
+function formatDifferenceTime(date1, date2) {
+    minutes = Math.abs(date2 - date1) / (60*1000);
+    hours = Math.floor(minutes / 60);
+    minutes = Math.floor(minutes % 60);
+    return hours + ":" + twoDigit(minutes);
 }
