53f20c1b8914f1026608ab33ed69aab6d97fba8d
Noah Kim <nokim@stonefly.mbhs.edu>
2016-04-07 21:50:24 -0400

    Added static javascript.

--- scout/settings.py
+++ scout/settings.py
@@ -121,4 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/scout/static/'
+
+STATICFILES_DIRS = [
+    '/var/www/scout/static'
+]
